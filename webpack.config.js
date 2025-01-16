069ba5aa9d0c6be75af6573c2c9300e5bbadf7a9
Renee Toscan <renee.toscan@outlook.com>
2025-01-15 16:22:30 -0800

    improving gitignore

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const htmlPlugin = new HtmlWebPackPlugin({
+  templateContent:
+    '<html><head></head><body><div id="root"></div></body></html>',
+});
+
+module.exports = {
+  // mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 8080,
+  },
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  ignoreWarnings: [
+    {
+      module: /module2\.js\?[34]/, // A RegExp
+    },
+    {
+      module: /[13]/,
+      message: /homepage/,
+    },
+    /warning from compiler/,
+    (warning) => true,
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: { loader: "ts-loader" },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" },
+      // {
+      //   test: /\.png|svg|jpg|gif$/,
+      //   use: ["file-loader"],
+      // },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: "source-map"
+};
